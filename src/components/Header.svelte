4c1e7cf4b101d31660917b7b359a228a962fa5ea
Vagaro <pajaromen@gmail.com>
2020-12-14 14:41:32 -0500

    chore: Added basic layout with zea-web-components

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -12,7 +12,7 @@
 
 <div class="flex items-center w-full h-full px-2">
   <div class="logo flex items-center h-full mr-4">
-    <img src="https://cdn.glitch.com/902123f3-ae96-4804-b014-ff54744cf91a%2Flogo-zea.svg?v=1600384733817" alt="logo">
+    <img src="./images/logo-zea.svg" alt="logo">
   </div>
   <zea-user-chip-set id="zea-user-chip-set"></zea-user-chip-set>
   <div class="divider"></div>
