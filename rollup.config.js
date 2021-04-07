ce08d4613bc7ee4308cb7954b607cc9eb75c2246
mauroporras <167700+mauroporras@users.noreply.github.com>
2021-04-07 14:25:39 -0500

    feat: add service worker

--- rollup.config.js
+++ rollup.config.js
@@ -8,6 +8,7 @@ import { copySync, removeSync } from 'fs-extra'
 import { spassr } from 'spassr'
 import getConfig from '@roxi/routify/lib/utils/config'
 import autoPreprocess from 'svelte-preprocess'
+import { injectManifest } from 'rollup-plugin-workbox'
 
 const { distDir } = getConfig() // use Routify's distDir for SSOT
 const assetsDir = 'assets'
@@ -80,6 +81,15 @@ export default {
         map: { mappings: '' },
       }),
     },
+    production &&
+      injectManifest({
+        globDirectory: assetsDir,
+        globPatterns: ['**/*.{js,css,svg}', '__app.html'],
+        swSrc: `src/sw.js`,
+        swDest: `${distDir}/serviceworker.js`,
+        maximumFileSizeToCacheInBytes: 10000000, // 10 MB,
+        mode: 'production',
+      }),
     production && copyToDist(),
   ],
   watch: {
--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,12 @@
 <script>
   import { Router } from '@roxi/routify'
   import { routes } from '../.routify/routes'
+  import Serviceworker from './Serviceworker.svelte'
 </script>
 
 <Router {routes} />
+
+<Serviceworker />
 
 <style global>
   @import '../assets/global.css';
