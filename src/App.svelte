4fe6ec43e21140d5817be7336528dca7fce163a5
mauroporras <167700+mauroporras@users.noreply.github.com>
2021-06-28 09:29:36 -0500

    fix: remove ServiceWorker

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,9 @@
 <script>
   import { Router } from '@roxi/routify'
   import { routes } from '../.routify/routes'
-  import Serviceworker from './Serviceworker.svelte'
 </script>
 
 <Router {routes} />
-
-<Serviceworker />
 
 <style global>
   @import '../assets/global.css';
